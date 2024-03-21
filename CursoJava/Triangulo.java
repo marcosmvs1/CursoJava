715ba1115cc14146ff8cacc9a81fe5e999a77e00
Marcos Vieira <145136000+marcosmvs1@users.noreply.github.com>
2024-03-21 09:12:23 -0300

    conf java

--- CursoJava/Triangulo.java
+++ CursoJava/Triangulo.java
@@ -1,19 +1,30 @@
 public class Geom2D {
-    double perimetro;
-    double calcPerimetro(){
+    protected double perimetro;
+
+    public double calcPerimetro() {
         return 0;
     }
 }
- 
-public class Triangulo extends Geom2D { 
-    ...
+
+public class Triangulo extends Geom2D {
+    private double ladoA;
+    private double ladoB;
+    private double ladoC;
+
+    public Triangulo(double ladoA, double ladoB, double ladoC) {
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.ladoC = ladoC;
+    }
+
     @Override
-    double calcPerimetro() {
+    public double calcPerimetro() {
         super.perimetro = this.ladoA + this.ladoB + this.ladoC;
         return super.perimetro;
     }
+
     public static void main(String[] args) {
         Triangulo trian = new Triangulo(3, 25, 26);
         System.out.println("Perimetro: " + trian.calcPerimetro());
     }
-} 
+}
